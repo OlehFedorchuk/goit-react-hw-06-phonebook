2d3fa70689adc0b28ad766b395ade516ee9873fe
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2023-09-01 18:19:30 +0100

    update

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,32 +1,12 @@
-// import React from 'react';
-// import { ContactItem } from './ContactItem/ContactItem';
-// import { useSelector } from 'react-redux';
 
-// export const ContactList = ({ onDeleteContact }) => {
-//   const contacts = useSelector(state => state.contacts);
-//   const filter = useSelector(state => state.filter);
-
-//   const filterContact = contacts.filter(contact =>
-//     contact.name.toLowerCase().includes(filter.toLowerCase().trim())
-//   );
-
-//   const contactElem = filterContact.map(contact => (
-//     <ContactItem
-//       key={contact.id}
-//       contact={contact}
-//       onDeleteContact={onDeleteContact}
-//     />
-//   ));
-
-//   return <div className='li'>{contactElem}</div>;
-// };
 import React from 'react';
 import { ContactItem } from './ContactItem/ContactItem';
 import { useSelector, useDispatch } from 'react-redux';
 import { deleteContacts } from 'redux/actions';
 
-export const ContactList = ({ filter }) => {
+export const ContactList = () => {
   const contacts = useSelector(state => state.contacts);
+  const filter = useSelector(state => state.filter);
   const dispatch = useDispatch();
 
   const filterContact = contacts.filter(contact =>
